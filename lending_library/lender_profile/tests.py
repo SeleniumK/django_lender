c8a444b4f4e7a655e0662aa0aec47539df1865f3
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2017-01-16 10:55:18 -0800

    testing scaffolding for profile model

--- lending_library/lender_profile/tests.py
+++ lending_library/lender_profile/tests.py
@@ -1,3 +1,11 @@
+"""Tests for the lender_profile app."""
+
 from django.test import TestCase
 
-# Create your tests here.
+
+class ProfileTestCase(TestCase):
+    """The PatronProfile Model test runner."""
+
+    def test_foo(self):
+        """Example Test."""
+        self.assertTrue(False)
