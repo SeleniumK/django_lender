832fb453d68ba30d0b89613ec1ee9f3cc7abc115
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2017-01-16 10:32:56 -0800

    Fixed required field on PatronProfileModel and set up scaffolding for a post save signal

--- lending_library/lender_profile/models.py
+++ lending_library/lender_profile/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class PatronProfile(models.Model):
     """Model representing User Profile (Patron Profile)."""
@@ -11,6 +14,12 @@ class PatronProfile(models.Model):
         related_name="profile",
         on_delete=models.CASCADE
     )
-    money_owed = models.DecimalField(max_digits=8, decimal_places=2)
+    money_owed = models.DecimalField(max_digits=8, decimal_places=2, blank=True)
     address = models.CharField(max_length=255, blank=True, null=True)
     library_id = models.UUIDField(default=uuid.uuid4, editable=False)
+
+
+@receiver(post_save, sender=User)
+def make_user_profile(sender, instance, **kwargs):
+    """Create Patron Profile after new User is saved."""
+    pass
